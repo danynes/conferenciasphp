3d79078c9d988b8cfb82d2c4dd3e1c703d287e3d
nestor suarez <danynesmx@gmail.com>
2018-05-07 13:12:48 -0500

    nestor
    conexion a la bd. tabla evento

--- calendario.php
+++ calendario.php
@@ -9,6 +9,31 @@
     <section class="seccion contenedor">
     <h2>Calendario de Eventos</h2>
 
+    <?php
+
+    try {
+      require_once('includes/funciones/bd_conexion.php');
+      $sql ="SELECT * FROM evento";
+      $resultado = $conn -> query($sql);
+    } catch (\Exception $e) {
+      echo $e->getMessage();
+    }
+
+    ?>
+    <div class="calendario">
+      <?php
+       $evento = $resultado-> fetch_assoc();
+
+       ?>
+       <pre>
+            <?php var_dump($evento); ?>
+       </pre>
+
+    </div>
+    <?php
+        $conn->close();
+     ?>
+
 
     </section>
 
