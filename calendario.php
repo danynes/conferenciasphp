af41768719194b40c4d4249157586c2447d62af6
nestor suarez <danynesmx@gmail.com>
2018-05-10 17:59:08 -0500

    nestor
    sacando ordenadamente los datos de la tabla

--- calendario.php
+++ calendario.php
@@ -14,7 +14,13 @@
 
               try {
                 require_once('includes/funciones/bd_conexion.php');
-                $sql ="SELECT * FROM eventos";
+                $sql =" SELECT evento_id,nombre_evento,fecha_evento,hora_evento,cat_evento,nombre_invitado,apellido_invitado ";
+                $sql .=" from eventos ";
+                $sql .=" inner join categoria_evento ";
+                $sql .=" on eventos.id_cat_evento = categoria_evento.id_categoria ";
+                $sql .=" inner join invitados ";
+                $sql .=" on eventos.id_inv = invitados.invitado_id ";
+                $sql .=" order by fecha_evento ";
                 $resultado = $conn->query($sql);
               } catch (\Exception $e) {
                 echo $e->getMessage();
@@ -23,11 +29,13 @@
         ?>
         <div class="calendario">
             <?php
-              $eventos = $resultado->fetch_assoc();
-             ?>
-             <pre>
-                  <?php var_dump($eventos); ?>
-             </pre>
+              while ($eventos = $resultado->fetch_assoc()){?>
+                <pre>
+                     <?php var_dump($eventos); ?>
+                </pre>
+
+
+            <<?php   }  ?>
         </div>
             <?php
                 $conn->close();
