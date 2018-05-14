6d3c1dc7d05480b5cda92382ff0ef2a3b1f9d28a
nestor suarez <danynesmx@gmail.com>
2018-05-14 15:29:03 -0500

    Revert "nestor"
    This reverts commit af41768719194b40c4d4249157586c2447d62af6.

--- calendario.php
+++ calendario.php
@@ -14,13 +14,7 @@
 
               try {
                 require_once('includes/funciones/bd_conexion.php');
-                $sql =" SELECT evento_id,nombre_evento,fecha_evento,hora_evento,cat_evento,nombre_invitado,apellido_invitado ";
-                $sql .=" from eventos ";
-                $sql .=" inner join categoria_evento ";
-                $sql .=" on eventos.id_cat_evento = categoria_evento.id_categoria ";
-                $sql .=" inner join invitados ";
-                $sql .=" on eventos.id_inv = invitados.invitado_id ";
-                $sql .=" order by fecha_evento ";
+                $sql ="SELECT * FROM eventos";
                 $resultado = $conn->query($sql);
               } catch (\Exception $e) {
                 echo $e->getMessage();
@@ -29,13 +23,11 @@
         ?>
         <div class="calendario">
             <?php
-              while ($eventos = $resultado->fetch_assoc()){?>
-                <pre>
-                     <?php var_dump($eventos); ?>
-                </pre>
-
-
-            <<?php   }  ?>
+              $eventos = $resultado->fetch_assoc();
+             ?>
+             <pre>
+                  <?php var_dump($eventos); ?>
+             </pre>
         </div>
             <?php
                 $conn->close();
