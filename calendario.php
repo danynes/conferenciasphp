49200df4b0628c9738b63137faea7188f9859b1f
nestor suarez <danynesmx@gmail.com>
2018-05-14 16:45:07 -0500

    nestor
    imprimiendo datos de fechas.

--- calendario.php
+++ calendario.php
@@ -14,7 +14,13 @@
 
               try {
                 require_once('includes/funciones/bd_conexion.php');
-                $sql ="SELECT * FROM eventos";
+                $sql =" SELECT evento_id,nombre_evento,fecha_evento,hora_evento,cat_evento,nombre_invitado,apellido_invitado ";
+                $sql .=" from eventos ";
+                $sql .=" inner join categoria_evento ";
+                $sql .=" on eventos.id_cat_evento = categoria_evento.id_categoria ";
+                $sql .=" inner join invitados ";
+                $sql .=" on eventos.id_inv = invitados.invitado_id ";
+                $sql .=" order by fecha_evento ";
                 $resultado = $conn->query($sql);
               } catch (\Exception $e) {
                 echo $e->getMessage();
@@ -23,11 +29,44 @@
         ?>
         <div class="calendario">
             <?php
-              $eventos = $resultado->fetch_assoc();
-             ?>
-             <pre>
-                  <?php var_dump($eventos); ?>
-             </pre>
+            $canlendario = array();
+              while ($eventos = $resultado->fetch_assoc()){
+
+                $fecha = $eventos['fecha_evento'];
+
+
+                $evento = array(
+                  'titulo' => $eventos['nombre_evento'],
+                  'fecha' => $eventos['fecha_evento'],
+                  'hora' => $eventos['hora_evento'],
+                  'categoria' => $eventos['cat_evento'],
+                  'invitado' => $eventos['nombre_invitado']." ".$eventos['apellido_invitado']
+                );
+                $calendario[$fecha] []=$evento;
+              ?>
+            <?php }  //while de fetch_assoc()?>
+
+
+            <?php
+            // imprime todo los Eventos
+            foreach ($calendario as $dia => $lista_eventos) {?>
+              <h3><i class="fa fa-calendar-alt"></i>
+                <?php
+                //linux
+                setlocale(LC_TIME, 'es_ES.UTF-8');
+                //windows
+                setlocale(LC_TIME, 'spanish');
+                echo strftime("%A, %d de %B del %Y", strtotime($dia) ); ?>
+              </h3>
+            <?php  }             ?>
+
+                <pre>
+                     <?php var_dump($calendario); ?>
+                </pre>
+
+
+
+
         </div>
             <?php
                 $conn->close();
